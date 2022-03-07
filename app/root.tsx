b8c52d2b30315fe5c0897cc1265783426b79eab9
Daniel Slanicka <mysiacik@mysiacik.com>
2022-03-07 22:40:08 +0100

    first commit

---
+++ app/root.tsx
@@ -0,0 +1,141 @@
+import {
+  Link,
+  Links,
+  LinksFunction,
+  LiveReload,
+  LoaderFunction,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  useCatch,
+  useLoaderData,
+} from 'remix';
+import type { MetaFunction } from 'remix';
+import styles from './tailwind.css';
+
+export const links: LinksFunction = () => [{ rel: 'stylesheet', href: styles }];
+
+export const meta: MetaFunction = () => {
+  return { title: 'New Remix App' };
+};
+
+export const loader: LoaderFunction = () => {
+  return {
+    env: {
+      SUPABASE_URL: process.env.SUPABASE_URL,
+      PUBLIC_SUPABASE_ANON_KEY: process.env.PUBLIC_SUPABASE_ANON_KEY,
+    },
+  };
+};
+
+export default function App() {
+  return (
+    <Document title="Remix template">
+      <Layout>
+        <Outlet />
+      </Layout>
+    </Document>
+  );
+}
+
+function Document({ children, title }: { children: React.ReactNode; title?: string }) {
+  const { env } = useLoaderData<Window>();
+
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        {title ? <title>{title}</title> : null}
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        {children}
+        <ScrollRestoration />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.env = ${JSON.stringify(env)}`,
+          }}
+        />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
+
+function Layout({ children }: React.PropsWithChildren<{}>) {
+  return (
+    <div className="flex flex-col justify-between h-screen">
+      <header className="mb-10 p-4 shadow-xl bg-gray-300"></header>
+      <main className="flex-grow">
+        <div className="">{children}</div>
+      </main>
+      <footer className="items-center p-4 bg-gray-300"></footer>
+    </div>
+  );
+}
+
+export function ErrorBoundary({ error }: { error: Error }) {
+  console.error(error);
+  return (
+    <Document title="Something went wrong!">
+      <div className="bg-white min-h-full px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
+        <div className="max-w-max mx-auto">
+          <main className="sm:flex">
+            <p className="text-4xl font-extrabold text-indigo-600 sm:text-5xl">500</p>
+            <div className="sm:ml-6">
+              <div className="sm:border-l sm:border-gray-200 sm:pl-6">
+                <h1 className="text-4xl font-extrabold text-gray-900 tracking-tight sm:text-5xl">
+                  Something went wrong!
+                </h1>
+                <p className="mt-1 text-base text-gray-500">Please check the URL in the address bar and try again.</p>
+              </div>
+              <div className="mt-10 flex space-x-3 sm:border-l sm:border-transparent sm:pl-6">
+                <Link
+                  to="/"
+                  className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  Go back home
+                </Link>
+              </div>
+            </div>
+          </main>
+        </div>
+      </div>
+    </Document>
+  );
+}
+
+export function CatchBoundary() {
+  let caught = useCatch();
+  return (
+    <Document title={caught.statusText}>
+      <div className="bg-white min-h-full px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
+        <div className="max-w-max mx-auto">
+          <main className="sm:flex">
+            <p className="text-4xl font-extrabold text-indigo-600 sm:text-5xl">{caught.status}</p>
+            <div className="sm:ml-6">
+              <div className="sm:border-l sm:border-gray-200 sm:pl-6">
+                <h1 className="text-4xl font-extrabold text-gray-900 tracking-tight sm:text-5xl">
+                  {caught.statusText}
+                </h1>
+                <p className="mt-1 text-base text-gray-500">Please check the URL in the address bar and try again.</p>
+              </div>
+              <div className="mt-10 flex space-x-3 sm:border-l sm:border-transparent sm:pl-6">
+                <Link
+                  to="/"
+                  className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  Go back home
+                </Link>
+              </div>
+            </div>
+          </main>
+        </div>
+      </div>
+    </Document>
+  );
+}
